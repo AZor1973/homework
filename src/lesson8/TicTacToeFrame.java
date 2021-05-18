8191abf60894aaeb4130d722a95a29c8a4386492
azor <azor@mail.ru>
2021-05-18 09:50:32 +0300

    Change TicTacToe

--- src/lesson8/TicTacToeFrame.java
+++ src/lesson8/TicTacToeFrame.java
@@ -2,15 +2,17 @@ package lesson8;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
+import java.util.Random;
 
 public class TicTacToeFrame extends JFrame {
-    protected static String command = "PLAY";
+    private static String command = "PLAY";
     private static final String GAME_OVER = "GAME_OVER";
-    protected static final String AI_TURN = "AI_TURN";
-    protected static final String PAUSE = "PAUSE";
+    private static final String AI_TURN = "AI_TURN";
+    private static final String PAUSE = "PAUSE";
     private static String result = "play";
-    protected static int x;
-    protected static int y;
+    private static int x = 0;
+    private static int y = 0;
     private static final int size = TicTacToeLogic.getSize();
     private static final JButton[][] arrButtons = new JButton[size][size];
 
@@ -29,7 +31,7 @@ public class TicTacToeFrame extends JFrame {
                         addActionListener(e -> {
                             x = getX() / 97;
                             y = (getY() - 1) / 92;
-                            if (TicTacToeLogic.map[y][x] == TicTacToeLogic.getDot_EMPTY()) {
+                            if (TicTacToeLogic.getMap()[y][x] == TicTacToeLogic.getDot_EMPTY()) {
                                 setText("X");
                                 setForeground(Color.BLUE);
                             }
@@ -46,8 +48,8 @@ public class TicTacToeFrame extends JFrame {
                             }
                             if (command.equals(AI_TURN)) {
                                 TicTacToeLogic.aiTurn();
-                                arrButtons[TicTacToeLogic.yOut][TicTacToeLogic.xOut].setText("O");
-                                arrButtons[TicTacToeLogic.yOut][TicTacToeLogic.xOut].setForeground(Color.RED);
+                                arrButtons[TicTacToeLogic.getYOut()][TicTacToeLogic.getXOut()].setText("O");
+                                arrButtons[TicTacToeLogic.getYOut()][TicTacToeLogic.getXOut()].setForeground(Color.RED);
                                 if (TicTacToeLogic.checkWin(TicTacToeLogic.getDotO())) {
                                     result = "Победил ИИ";
                                     System.out.println(result);
@@ -101,6 +103,196 @@ public class TicTacToeFrame extends JFrame {
             }
         });
 
+    }
+    
+    private static class TicTacToeLogic {
+        private static final int SIZE = 5;
+        private static final int DOTS_TO_WIN = 4;
+        private static final char DOT_EMPTY = '•';
+        private static final char DOT_X = 'X';
+        private static final char DOT_O = 'O';
+        private static final char[][] map = new char[SIZE][SIZE];
+        private static final Random rand = new Random();
+        private static int xOut;
+        private static int yOut;
+
+        //    checkWin проверяет по строкам, столбцам, главным и второстепенным диагоналям
+        protected static boolean checkWin(char c) {
+            if (checkRowColumn(c)) return true;
+            if (checkMinorDiagonal(c)) return true;
+            return checkMainDiagonal(c);
+        }
+
+        private static boolean checkRowColumn(char c) {
+            for (int i = 0; i < SIZE; i++) {
+                int countRow = 0;
+                int countColumn = 0;
+                for (int j = 0; j < SIZE; j++) {
+                    countRow = (map[i][j] == c) ? countRow + 1 : 0;
+                    countColumn = (map[j][i] == c) ? countColumn + 1 : 0;
+                    if (countRow == DOTS_TO_WIN || countColumn == DOTS_TO_WIN) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static boolean checkMainDiagonal(char c) {
+            int countDiagonal = 0;
+            int countRevDiagonal = 0;
+            for (int i = 0; i < SIZE; i++) {
+                countDiagonal = (map[i][i] == c) ? countDiagonal + 1 : 0;
+                countRevDiagonal = (map[i][map.length - 1 - i] == c) ? countRevDiagonal + 1 : 0;
+                if (countDiagonal == DOTS_TO_WIN || countRevDiagonal == DOTS_TO_WIN) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Вроде работает
+        private static boolean checkMinorDiagonal(char c) {
+            int[] countDiagonal = new int[(SIZE - DOTS_TO_WIN) * 4]; // кол-во второстепенных диагоналей зависит от SIZE и DOT_TO_WIN
+            for (int k = 0, i = 1; k < countDiagonal.length; k += 4, i++) {
+                for (int j = 0; j < SIZE - i; j++) {
+                    // Выявил зависимость, но упростить не получилось
+                    countDiagonal[k] = (map[j][(SIZE - 1) - i - j] == c) ? countDiagonal[k] + 1 : 0;
+                    countDiagonal[k + 1] = (map[j + i][(SIZE - 1) - j] == c) ? countDiagonal[k + 1] + 1 : 0;
+                    countDiagonal[k + 2] = (map[j + i][j] == c) ? countDiagonal[k + 2] + 1 : 0;
+                    countDiagonal[k + 3] = (map[j][j + i] == c) ? countDiagonal[k + 3] + 1 : 0;
+                    for (int value : countDiagonal) {
+                        if (value == DOTS_TO_WIN) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        protected static boolean isMapFull() {
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (map[i][j] == DOT_EMPTY) return false;
+                }
+            }
+            return true;
+        }
+
+        // Контролирует последний выигрышный ход и свой, и соперника
+        protected static void aiTurn() {
+            int x, y;
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (map[i][j] == DOT_EMPTY) {
+                        map[i][j] = DOT_O;
+                        if (checkWin(DOT_O)) {
+                            System.out.println("Компьютер походил в точку " + (j + 1) + " " + (i +
+                                    1));
+                            xOut = j;
+                            yOut = i;
+                            printMap();
+                            command = PAUSE;
+                            return;
+                        } else {
+                            map[i][j] = DOT_X;
+                            if (checkWin(DOT_X)) {
+                                System.out.println("Компьютер походил в точку " + (j + 1) + " " + (i +
+                                        1));
+                                map[i][j] = DOT_O;
+                                xOut = j;
+                                yOut = i;
+                                printMap();
+                                command = PAUSE;
+                                return;
+                            } else {
+                                map[i][j] = DOT_EMPTY;
+                            }
+                        }
+                    }
+                }
+            }
+            do {
+                x = rand.nextInt(SIZE);
+                y = rand.nextInt(SIZE);
+            } while (map[y][x] != DOT_EMPTY);
+            System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y +
+                    1));
+            map[y][x] = DOT_O;
+            xOut = x;
+            yOut = y;
+            printMap();
+            command = PAUSE;
+        }
+
+
+        protected static void humanTurn() {
+            int x = TicTacToeFrame.x, y = TicTacToeFrame.y;
+            if (map[y][x] == DOT_EMPTY) {
+                System.out.println("Человек пошёл.");
+                map[y][x] = DOT_X;
+                printMap();
+                command = AI_TURN;
+            }
+        }
+
+        protected static void initMap() {
+            for (int i = 0; i < SIZE; i++) {
+                Arrays.fill(map[i], DOT_EMPTY);
+            }
+        }
+
+        protected static void printMap() {
+            printHeader();
+            printBody();
+            System.out.println();
+        }
+
+        private static void printHeader() {
+            for (int i = 0; i <= SIZE; i++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+
+        private static void printBody() {
+            for (int i = 0; i < SIZE; i++) {
+                System.out.print((i + 1) + " ");
+                for (int j = 0; j < SIZE; j++) {
+                    System.out.print(map[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+
+        public static int getSize() {
+            return SIZE;
+        }
+
+        public static char getDotX() {
+            return DOT_X;
+        }
+
+        public static char getDotO() {
+            return DOT_O;
+        }
+
+        public static char getDot_EMPTY() {
+            return DOT_EMPTY;
+        }
+
+        public static char[][] getMap() {
+            return map;
+        }
+
+        public static int getXOut() {
+            return xOut;
+        }
+
+        public static int getYOut() {
+            return yOut;
+        }
     }
 }
 
